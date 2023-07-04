40a3f575830fc07892d5ad834be4c1fc70bb1b4a
abdou50 <gadourraddadi@gmail.com>
2023-07-04 18:07:38 +0100

    l

--- 0x04-nary_trees/nary_trees.h
+++ 0x04-nary_trees/nary_trees.h
@@ -1,7 +1,6 @@
 #ifndef NARY_TREES_H
 #define NARY_TREES_H
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 /**
  * struct nary_tree_s - N-ary tree node structure
@@ -14,15 +13,18 @@
  */
 typedef struct nary_tree_s
 {
-    char            *content;
-    struct nary_tree_s  *parent;
-    size_t          nb_children;
-    struct nary_tree_s  *children;
-    struct nary_tree_s  *next;
+	char *content;
+	struct nary_tree_s *parent;
+	size_t nb_children;
+	struct nary_tree_s *children;
+	struct nary_tree_s *next;
 } nary_tree_t;
+
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str);
 void nary_tree_delete(nary_tree_t *tree);
-size_t nary_tree_traverse(nary_tree_t const *root, void (*action)(nary_tree_t const *node, size_t depth));
+size_t nary_tree_traverse(nary_tree_t const *root,
+		void (*action)(nary_tree_t const *node, size_t depth));
+size_t nary_tree_depth(nary_tree_t const *root);
 size_t nary_tree_diameter(nary_tree_t const *root);
-int path_exists(nary_tree_t const *root, char const * const *path);
+int path_exists(nary_tree_t const *root, char const *const *path);
 #endif
