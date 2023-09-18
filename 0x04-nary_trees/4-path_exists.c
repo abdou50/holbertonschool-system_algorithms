15fd76ede4c2a65810ad7f7cc74b111c357d3e1d
abdou50 <gadourraddadi@gmail.com>
2023-09-18 22:18:40 +0100

    l

--- 0x04-nary_trees/4-path_exists.c
+++ 0x04-nary_trees/4-path_exists.c
@@ -1,24 +1,23 @@
 #include "nary_trees.h"
-#include <string.h>
+
 /**
- * path_exists - checks if a path exists
- * @root: the root 
- * @path: the path
- * Return: 1 or 0
+ * path_exists - Checks if a path exists between two nodes
+ * @root: Pointer to the root node
+ * @path: Pointer to the path
+ * Return: 1 if path exists, 0 otherwise
  */
-int path_exists(nary_tree_t const *root, char const *const *path)
+int path_exists(nary_tree_t const *root, char const * const *path)
 {
-	nary_tree_t *tmp;
+	size_t i;
 
-	if (!root || !path || strcmp(root->content, *path))
+	if (!root || !path || !*path)
 		return (0);
-
-	if (*(++path) == NULL)
-		return (1);
-
-	for (tmp = root->children; tmp; tmp = tmp->next)
-		if (path_exists(tmp, path))
-			return (1);
-
-	return (0);
+	for (i = 0; path[i]; ++i, root = root->children)
+	{
+		while (root && strcmp(root->content, path[i]) != 0)
+			root = root->next;
+		if (!root)
+			return (0);
+	}
+	return (1);
 }
